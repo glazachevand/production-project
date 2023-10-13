0edfe77a9d6869ff406a2db86bda08ee23d1c473
glazachevand <111851786+glazachevand@users.noreply.github.com>
2023-10-13 12:46:13 +0700

    add eslint, correct package.json

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -4,12 +4,7 @@ import { useTranslation } from "react-i18next";
 const AboutPage: React.FC = () => {
   const { t } = useTranslation("about");
 
-  return (
-    <div>
-      {t("О сайте")}
-      {t("О нашем айте")}
-    </div>
-  );
+  return <div>{t("О сайте")} Страница</div>;
 };
 
 export default AboutPage;
