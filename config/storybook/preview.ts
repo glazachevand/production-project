41cd2671999627e655b0d49688a72f90cc089e8b
glazachevand <111851786+glazachevand@users.noreply.github.com>
2023-10-21 21:47:28 +0700

    delete correct

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,13 +1,8 @@
 import type { Preview } from '@storybook/react';
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
-// import { Theme } from '../../src/app/providers/ThemeProvider/lib/ThemeContext';
+import { Theme } from '../../src/app/providers/ThemeProvider';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
-
-const enum Theme {
-  LIGHT = 'light',
-  DARK = 'dark',
-}
 
 const preview: Preview = {
   parameters: {
