248a648316cf55a9e3c7e3a18a2c805bf0ec329e
glazachevand <111851786+glazachevand@users.noreply.github.com>
2023-10-21 20:22:27 +0700

    correct themeprovider

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,7 +1,7 @@
 import type { Preview } from '@storybook/react';
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import { Theme } from '../../src/app/providers/ThemeProvider';
+import { Theme } from '../../src/app/providers/ThemeProvider/ui/ThemeProvider';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
 
 const preview: Preview = {
