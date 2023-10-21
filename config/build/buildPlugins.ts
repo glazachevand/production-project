08facf3f920ae6b53956c5248145034f914b8a2c
glazachevand <111851786+glazachevand@users.noreply.github.com>
2023-10-21 17:28:20 +0700

    fix dev plagins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -7,7 +7,7 @@ import { BuildOptions } from './types/config';
 
 // возвращает список плагинов
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -19,10 +19,14 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-    new ReactRefreshWebpackPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
   ];
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }));
+  }
+
+  return plugins;
 }
