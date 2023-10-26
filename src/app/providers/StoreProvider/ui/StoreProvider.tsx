7b807cb6aacb79c6a383a8232b10e1706b068515
glazachevand <111851786+glazachevand@users.noreply.github.com>
2023-10-26 10:45:33 +0700

    correct Modal

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -6,16 +6,16 @@ import { DeepPartial } from '@reduxjs/toolkit';
 
 interface StoreProviderProps {
   children?: ReactNode;
-    initialState?: DeepPartial<StateSchema>;
+  initialState?: DeepPartial<StateSchema>;
 }
 
 export const StoreProvider = (props: StoreProviderProps) => {
-    const {
-        children,
-        initialState,
-    } = props;
+  const {
+    children,
+    initialState,
+  } = props;
 
-    const store = createReduxStore(initialState as StateSchema);
+  const store = createReduxStore(initialState as StateSchema);
 
   return (
     <Provider store={store}>
