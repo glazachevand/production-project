85528bad0a6f6cd237bce477c7bceaa35578e9d7
glazachevand <111851786+glazachevand@users.noreply.github.com>
2023-10-27 11:56:44 +0700

    add Input

---
+++ src/entities/User/index.ts
@@ -0,0 +1,6 @@
+import { userReducer, userActions } from './model/slice/UserSlice';
+import { UserSchema, User } from './model/types/user';
+
+export {
+  userReducer, userActions, UserSchema, User,
+};
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,20 @@
 import { Counter } from 'entities/Counter';
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Input } from 'shared/ui/Input/Input';
 
 const MainPage: React.FC = () => {
   const { t } = useTranslation('main');
+  const [value, setValue] = useState('');
+
+  const onChange = (val: string) => {
+    setValue(val);
+  };
 
   return (
     <div>
       {t('Главная страница')}
+      {/* <Input value={value} onChange={onChange} placeholder={t('Введите текст')} /> */}
       {/* <Counter /> */}
     </div>
   );
